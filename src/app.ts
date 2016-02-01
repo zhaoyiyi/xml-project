d96f7e63547f62712fb7244bf3b1707b3e55656a
Yi Zhao <zhao1max@gmail.com>
2016-02-01 12:54:29 -0500

    use one stream instead of many

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
 
-
 import {RouteComponent} from './bus/route.component';
 import {MapComponent} from './map/map.component';
 import * as Rx from 'rxjs/Rx';
