d1b0a5cb8dbbe309d3c8cd939938b80d2a748d7e
Yi Zhao <zhao1max@gmail.com>
2016-05-04 12:19:01 -0400

    move to server update

--- src/boot.ts
+++ src/boot.ts
@@ -1,5 +1,8 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {App} from './app';
+import { bootstrap } from 'angular2/platform/browser';
+import { MATERIAL_BROWSER_PROVIDERS } from 'ng2-material/all';
+import { App } from './app';
 
 
-bootstrap(App);
+bootstrap(App, [
+  MATERIAL_BROWSER_PROVIDERS
+]);
