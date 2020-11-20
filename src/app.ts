decf0df4b8ff85e05e5fc7f834adbcde580d41fb
Yi Zhao <yzhao@hubba.com>
2020-11-20 16:58:20 -0500

    add dev script

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import { Component } from 'angular2/core';
 import { RouteComponent } from './bus/route.component';
-import { StopsComponent } from './stops/stops.component.ts';
+import { StopsComponent } from './stops/stops.component';
 import { MapComponent } from './map/map.component';
 import { MapService } from './map/map.service';
 import { RouteService } from './bus/route.service';
 import { HTTP_PROVIDERS } from 'angular2/http';
-import { MATERIAL_DIRECTIVES, Media, SidenavService } from "ng2-material/all";
+import { MATERIAL_DIRECTIVES, Media, SidenavService } from 'ng2-material/all';
 import 'rxjs/add/operator/pluck';
 
 @Component({
@@ -13,39 +13,54 @@ import 'rxjs/add/operator/pluck';
   template: `
     <div layout="column">
       <md-sidenav-container layout="row" flex style="padding-left: 450px;">
-        <md-sidenav name="left" align="left" layout="column" [style]="'side'" style="width: 450px; max-width: 500px;"  layout-padding>
+        <md-sidenav
+          name="left"
+          align="left"
+          layout="column"
+          [style]="'side'"
+          style="width: 450px; max-width: 500px;"
+          layout-padding
+        >
           <md-toolbar class="md-theme-indigo">
             <h1 class="md-toolbar-tools">Real time TTC</h1>
           </md-toolbar>
           <md-content style="overflow-y: auto" layout-padding flex>
             <div layout="column" layout-fill layout-align="center">
-              <route (routeChange)="onRouteChange($event)">
-              </route>
-              <stops (routeChange)="onRouteChange($event)">
-              </stops>
+              <route (routeChange)="onRouteChange($event)"> </route>
+              <stops (routeChange)="onRouteChange($event)"> </stops>
             </div>
           </md-content>
         </md-sidenav>
         <md-content flex>
           <div layout="column" layout-fill layout-align="top center">
-            <map [routeInfoStream]="routeInfoStream" [locationStream]="locationStream" [testStream]="testStream"></map>
+            <map
+              [routeInfoStream]="routeInfoStream"
+              [locationStream]="locationStream"
+              [testStream]="testStream"
+            ></map>
           </div>
         </md-content>
       </md-sidenav-container>
     </div>
-    
   `,
   providers: [SidenavService, MapService, RouteService, HTTP_PROVIDERS],
-  directives: [RouteComponent, MapComponent, StopsComponent, MATERIAL_DIRECTIVES],
-  inputs: ['routeChange']
+  directives: [
+    RouteComponent,
+    MapComponent,
+    StopsComponent,
+    MATERIAL_DIRECTIVES,
+  ],
+  inputs: ['routeChange'],
 })
 export class App {
   // take path coords emitted by route component and pass it to map component
-  public routeInfoStream:any;
-  public locationStream:any;
+  public routeInfoStream: any;
+  public locationStream: any;
 
-  constructor(private _sidenavService:SidenavService,
-              private _routeService:RouteService) {
+  constructor(
+    private _sidenavService: SidenavService,
+    private _routeService: RouteService
+  ) {
     // _sidenavService.show('left');
   }
 
@@ -54,5 +69,4 @@ export class App {
     this.locationStream = this._routeService.getBusLocations(routeNum);
     console.log('sending route info...');
   }
-
 }
--- src/map/map.service.ts
+++ src/map/map.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, Inject } from 'angular2/core';
-import * as Rx from 'rxjs/Rx';
+import { Injectable } from 'angular2/core';
 import { ICONSET } from './icon';
-import { zoom, addToBound, clear, clearMarker, animateMarker, addMarker } from './mapHelper';
+import {
+  zoom,
+  addToBound,
+  clear,
+  clearMarker,
+  animateMarker,
+  addMarker,
+} from './mapHelper';
 import { getCurrentLocation } from './location';
-import { Observable } from "rxjs/Observable";
+import { Observable } from 'rxjs/Observable';
 import { StopPrediction } from '../interface';
 declare var google, window;
 
@@ -30,11 +36,11 @@ export class MapService {
     if (clearPath) this.clearLine();
     // clears bounds
     if (this._bound) this._bound = new google.maps.LatLngBounds();
-    this._lines = routeInfo.map(path => {
+    this._lines = routeInfo.map((path) => {
       let line = new google.maps.Polyline(ICONSET.line(path));
       line.setMap(this._map);
       //path.map(point => this.addToBound(point));
-      path.map(point => addToBound(point, this._bound));
+      path.map((point) => addToBound(point, this._bound));
       //this.zoom();
       zoom(this._map, this._bound);
       return line;
@@ -53,7 +59,12 @@ export class MapService {
       this._buses.map((bus, idx) => {
         if (newPosition[idx] && bus.id === newPosition[idx].id) {
           animateMarker(bus.marker, newPosition[idx], 8000);
-          bus.marker.setIcon(ICONSET.bus(google.maps.SymbolPath.FORWARD_CLOSED_ARROW, newPosition[idx]));
+          bus.marker.setIcon(
+            ICONSET.bus(
+              google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+              newPosition[idx]
+            )
+          );
         }
       });
     } else {
@@ -64,9 +75,13 @@ export class MapService {
   public drawBuses(buses) {
     if (this._buses) clearMarker(this._buses);
     console.log('first time drawing buses for new route:');
-    this._buses = buses.map(bus => {
+    this._buses = buses.map((bus) => {
       if (bus && bus.id) {
-        return addMarker(this._map, bus, ICONSET.bus(google.maps.SymbolPath.FORWARD_CLOSED_ARROW, bus));
+        return addMarker(
+          this._map,
+          bus,
+          ICONSET.bus(google.maps.SymbolPath.FORWARD_CLOSED_ARROW, bus)
+        );
       }
     });
   }
@@ -76,12 +91,18 @@ export class MapService {
   public drawStops(stops: Array<StopPrediction>, infoContent?: Array<string>) {
     if (this._stops) clearMarker(this._stops);
     this._stops = stops.map((stop, index) => {
-      const stop = addMarker(this._map, stop, ICONSET.stop(google.maps.SymbolPath.CIRCLE));
+      const stop = addMarker(
+        this._map,
+        stop,
+        ICONSET.stop(google.maps.SymbolPath.CIRCLE)
+      );
       if (infoContent) {
         const info = new google.maps.InfoWindow({
-          content: infoContent[index]
+          content: infoContent[index],
         });
-        stop.marker.addListener('click', () => info.open(this._map, stop.marker));
+        stop.marker.addListener('click', () =>
+          info.open(this._map, stop.marker)
+        );
       }
       return stop;
     });
@@ -92,7 +113,6 @@ export class MapService {
     this._map.setZoom(18);
   }
 
-
   // init //
   public loadMap(mapName) {
     let script = document.createElement('script');
@@ -102,19 +122,19 @@ export class MapService {
     script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAlWFKOQSQvQx2Xr9qw7i8siK7ktQlGcco&callback=initMap&libraries=places`;
     document.body.appendChild(script);
     // attach initMap to window
-    (window)['initMap'] = () => this.initMap(mapName);
+    window['initMap'] = () => this.initMap(mapName);
   }
 
   // init callback
   private initMap(mapName) {
     this._map = new google.maps.Map(document.querySelector(mapName), {
       center: { lat: 43.646389, lng: -79.408959 },
-      zoom: 15
+      zoom: 15,
     });
 
     this._bound = new google.maps.LatLngBounds();
     this._placeService = new google.maps.places.PlacesService(this._map);
-    getCurrentLocation().subscribe(pos => {
+    getCurrentLocation().subscribe((pos) => {
       addMarker(this._map, pos, ICONSET.me(google.maps.SymbolPath.CIRCLE));
       // set current location
       this._currentLocation = pos;
@@ -123,5 +143,3 @@ export class MapService {
     });
   }
 }
-
-
